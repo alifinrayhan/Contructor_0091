4f27f53ce904d6f3be671524c34cdf5b58cf94fd
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-13 14:47:07 +0700

    void mahasiswa

--- Contructor 1.cpp
+++ Contructor 1.cpp
@@ -21,3 +21,15 @@ public:
       
 };
 
+int mahasiswa::nim = 20;
+void mahasiswa::setId()
+{
+    Id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+    cout << " ID = " << Id << endl;
+    cout << " Nama = " << nama << endl;
+    cout << endl;
+}
